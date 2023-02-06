e082b03eee59b21cd54205630d368fab50d759ce
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-02-06 19:07:09 -0300

    Realiza requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import getMusics from '../services/musicsAPI';
+
+class MusicCard extends React.Component {
+  state = {
+    musics: [],
+  };
+
+  componentDidMount() {
+    this.atualizaEstado();
+  }
+
+  atualizaEstado = async () => {
+    const { id } = this.props;
+    const musics = await getMusics(id);
+    const tracks = await musics.filter((element) => element.kind === 'song');
+    this.setState({
+      musics: [tracks],
+    });
+  };
+
+  render() {
+    const { musics } = this.state;
+    console.log(musics);
+    return (
+      <div>
+        {
+          musics.length > 0 && (musics[0].map((music, index) => (
+            <div key={ index }>
+              <h4>
+                {music.trackName}
+              </h4>
+              <audio data-testid="audio-component" src={ music.previewUrl } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                {' '}
+                <code>audio</code>
+              </audio>
+            </div>
+          )))
+
+        }
+      </div>
+    );
+  }
+}
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  state = {
+    artistName: '',
+    collectionName: '',
+    musics: [],
+  };
+
+  componentDidMount() {
+    this.requestApi();
+  }
+
+  requestApi = async () => {
+    const { id } = this.props.match.params;
+    const musics = await getMusics(id);
+    const tracks = await musics.filter((element) => element.wrapperType === 'track');
+    this.setState({
+      artistName: [musics[0].artistName],
+      collectionName: [musics[0].collectionName],
+      musics: [tracks],
+    });
+  };
+
   render() {
+    const { id } = this.props.match.params;
+    const { artistName, collectionName, musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <h1>Album</h1>
+        <h2 data-testid="artist-name">{ artistName }</h2>
+        <h3 data-testid="album-name">{ collectionName }</h3>
+        <MusicCard id={ id } />
       </div>
     );
   }
